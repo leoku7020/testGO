22c18232af47760fa3187b85f00ca9b5dea7d954
Eric <eric@Ericde-MacBook-Pro.local>
2019-11-26 15:17:44 +0800

    fix error message error text

--- testing/cal.go
+++ testing/cal.go
@@ -6,7 +6,7 @@ import (
 
 func Division(a, b float64) (float64, error) {
     if b == 0 {
-        return 0, errors.New("除鼠不可為0")
+        return 0, errors.New("除數不可為0")
     }
 
     return a / b, nil
